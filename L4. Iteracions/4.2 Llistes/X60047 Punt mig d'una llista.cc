00b46d88c27d56936ece3089c685900c56eaa2f0
MUX-enjoyer <roger.bitlloch@estudiantat.upc.edu>
2025-03-12 00:22:23 +0100

    indent updat

--- L4. Iteracions/4.2 Llistes/X60047 Punt mig d'una llista.cc
+++ L4. Iteracions/4.2 Llistes/X60047 Punt mig d'una llista.cc
@@ -11,53 +11,35 @@ int main() {
 
     while (cin >> instruccio) {
         if (instruccio == "get_mid_value") {
-            if (size % 2 == 0)
-                cout << "error" << endl;
-            else
-                cout << *mid << endl;
+            if (size % 2 == 0) cout << "error" << endl;
+            else cout << *mid << endl;
         } else if (instruccio == "push_front") {
             cin >> x;
             llista.push_front(x);
             ++size;
-            if (size == 1) {
-                mid = llista.begin();
-            } else if (size % 2 == 1) {
-                --mid;
-            }
+            if (size == 1) mid = llista.begin();
+            else if (size % 2 == 1) --mid;
         } else if (instruccio == "push_back") {
             cin >> x;
             llista.push_back(x);
             ++size;
-            if (size == 1) {
-                mid = llista.begin();
-            } else if (size % 2 == 0) {
-                ++mid;
-            }
+            if (size == 1) mid = llista.begin();
+            else if (size % 2 == 0) ++mid;
         } else if (instruccio == "pop_front") {
-            if (llista.empty()) {
-                cout << "error" << endl;
-            } else {
-                if (size % 2 == 1) {
-                    ++mid;
-                }
+            if (llista.empty()) cout << "error" << endl;
+            else {
+                if (size % 2 == 1) ++mid;
                 llista.pop_front();
                 --size;
-                if (size == 0) {
-                    mid = llista.end();
-                }
+                if (size == 0) mid = llista.end();
             }
         } else if (instruccio == "pop_back") {
-            if (llista.empty()) {
-                cout << "error" << endl;
-            } else {
-                if (size % 2 == 0) {
-                    --mid;
-                }
+            if (llista.empty()) cout << "error" << endl;
+            else {
+                if (size % 2 == 0) --mid;
                 llista.pop_back();
                 --size;
-                if (size == 0) {
-                    mid = llista.end();
-                }
+                if (size == 0) mid = llista.end();
             }
         }
     }
