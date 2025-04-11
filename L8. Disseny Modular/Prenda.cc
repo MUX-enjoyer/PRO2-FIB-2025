2bb2a0853df2033657874382e5ad1b6756bbd689
MUX-enjoyer <roger.bitlloch@estudiantat.upc.edu>
2025-04-11 11:27:27 +0200

    L8. X64677

---
+++ L8. Disseny Modular/Prenda.cc
@@ -0,0 +1,36 @@
+#include "Prenda.hh"
+
+Prenda::Prenda()
+{
+  peso = 0;
+  color = false;
+}
+
+Prenda::Prenda(int pes, bool col)
+{
+  peso = pes;
+  color = col;
+}
+
+void Prenda::modificar(int pes, bool col)
+{
+  peso = pes;
+  color = col;
+}
+
+int Prenda::consul_peso() const
+{
+  return peso;
+}
+
+bool Prenda::consul_color() const
+{
+  return color;
+}
+
+void Prenda::escribir() const
+{
+  cout << peso << ' ';
+  if (color) cout << "true" << endl;
+  else cout << "false" << endl;
+}
