e3c3facdba2d9e0b6208bfbf6cc546d45799272f
MUX-enjoyer <roger.bitlloch@estudiantat.upc.edu>
2025-04-11 11:32:54 +0200

    X19702 fix

--- Exàmens SOLUCIONATS/Parcial 1/2023 (SIMULACRE) TARDOR/P3: X19702 Llistes amb accés i inserció per índex/X19702 Llistes amb accés i inserció per índex.cc
+++ Exàmens SOLUCIONATS/Parcial 1/2023 (SIMULACRE) TARDOR/P3: X19702 Llistes amb accés i inserció per índex/X19702 Llistes amb accés i inserció per índex.cc
@@ -1,77 +1,41 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#include <list>
-
-// NO VA
-
-void imprimeix(list<int> l, list<int>::iterator it2, int index) {
-    list<int>::iterator it;
-    cout << "Llista";
-    for (it = l.begin(); it != l.end(); ++it) {
-        cout << " " << *it;
-    }
-    cout << endl << "Index " << index << *it2 << endl;
-}
-
-
 int main() {
-    list<int> v;
-    list<int>::iterator it = v.begin();
+    vector<int> v;
     int index = 0;
     string command;
     while (cin >> command) {
-        if (command == "index++;") ++it;
+        if (command == "index++;") {
+            ++index;
+        }
         else if (command == "index--;") {
-            if (v.size() == 1) {
-                it = v.begin();
-                index = 0;
-            }
-            else {
-                --it;
-                --index;
-            }
+            --index;
         }
         else if (command == "v.push_front(") {
             int number;
             cin >> number;
             string ending;
             cin >> ending; // Això consumeix el ");"
-            
-            v.push_front(number);
-            if (v.size() == 1) {
-                it = v.begin();
-                index = 0;
-            }
-            else {
-                --it;
-                --index;
-            }
+            v.insert(v.begin(),number);
         }
         else if (command == "v.push_back(") {
             int number;
             cin >> number;
             string ending;
             cin >> ending; // Això consumeix el ");"
-            
             v.push_back(number);
-            if (v.size() == 1) it = v.begin();
         }
         else if (command == "v.insert(index,") {
             int number;
             cin >> number;
             string ending;
             cin >> ending; // Això consumeix el ");"
-            
-            v.insert(it, number);
-            --it;
-            --index;
+            v.insert(v.begin() + index,number);
         }
-
         else if (command == "cout<<v[index]<<endl;") {
-            cout << *it << endl;
+            cout << v[index] << endl;
         }
-       // cout << command << endl;
-       // imprimeix(v, it, index);
     }
 }
